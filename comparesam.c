717960452621a1f401119345d7425791c2d1d418
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2018-12-06 23:21:16 +1100

    comments to comparesam

--- comparesam.c
+++ comparesam.c
@@ -21,34 +21,52 @@
 #include <iostream>
 using namespace std;
 
-#define NUM_MAPPINGS 300 //dont chnage this. only supports primary at the moment
+//maximum number of mappings for a particular read
+#define NUM_MAPPINGS 300  
+
+//SAM flags
 #define F_UNMAPPED 0x04
 #define F_SECONDARY 0x100
 #define F_SUPPLEMENTARY 0x800
 
+/*
+by default, only exact matches are taken to be correct (i.e. chr and the exact position)
+define OVERLAP_BASED_EVAL to allow an overlap
+*/
 //#define OVERLAP_BASED_EVAL 1
-#define CONSIDER_SUPPLEMENTARY 1
-#define CONSIDER_SECONDARY 1 //works only if supplemetary are considered
+
+/* 
+by default, only primary mappings are considered. define CONSIDER_SUPPLEMENTARY
+to consider supplementary reads set CONSIDER_SUPPLEMENTARY
+*/
+//#define CONSIDER_SUPPLEMENTARY 1
+
+/*
+by default, secondary mappings are ignored. define CONSIDER_SECONDARY to consider supplementary.
+however, this option works only if CONSIDER_SUPPLEMENTARY is also set
+*/
+//#define CONSIDER_SECONDARY 1 
+
+
+//used for debugging
 #define DEBUG_LOOP 1
 
+//SAM entry
 typedef struct{
-    string rid;
-    //int32_t query_start;
-    //int32_t query_end;
-    //int8_t strand;
-    string tid;
-    int32_t target_start;
-    int32_t target_end;
-    int32_t mapq;
-    int32_t score;
-    int32_t flag;
-    char strand; //for convenience
-
-    //string qual;
-
+    string rid; //read name
+    string tid; //reference sequence name
+    int32_t target_start; //mapping coordinate start
+    int32_t target_end; //mapping coordinate end
+    int32_t mapq; //MAPQ
+    int32_t score; //AS
+    int32_t flag; //sam flag
+    char strand; //+ or - strand
 }alignment_t;
 
+//sam file
 typedef struct{
+
+    //sam file statistics
     int32_t num_entries=0; //number of sam entries
 	int32_t mapped_reads=0; //total number of reads that map (primary)
 	int32_t mappings=0; //total number of alignments 
@@ -56,26 +74,31 @@ typedef struct{
     int32_t secondary=0;
     int32_t supplementary=0;
 
-    alignment_t mapping_of_reads[NUM_MAPPINGS];
+    //temporary data structure thats hold all the mappings for the current read
+    alignment_t mapping_of_reads[NUM_MAPPINGS]; 
     int32_t mapping_of_reads_n=0;
 
 } sam_stat_t;
 
 
+//global statistic of the sam comparison - between samfile 'a' and 'b'
 typedef struct{
 
-    int32_t unmapped_in_both=0;
-    int32_t same=0;
-    int32_t mismatches=0;
-    int32_t only_in_a=0;
-    int32_t only_in_b=0;
-    int32_t pri_a_to_supp_b=0;
-    int32_t pri_a_to_sec_b=0;
+    //number of reads that fall under each scenario
+    int32_t unmapped_in_both=0; //the read is unmapped in both 'a' and 'b'
+    int32_t same=0;             //correct - the read maps to the same location (locations overlaps if overlap based evaluation is set)
+    int32_t mismatches=0;       //in correct
+    int32_t only_in_a=0;        //read is only mapped in  'a'
+    int32_t only_in_b=0;        //read is only mapped in  'b'
+    int32_t pri_a_to_supp_b=0;  //primary mapping in 'a' is a supplementary mapping in 'b'
+    int32_t pri_a_to_sec_b=0;   //primary mapping in 'b' is a secondary mappings in 'b'
 
+    //file pointers for tsv
     FILE* f_only_in_a;
     FILE* f_only_in_b;
     FILE* f_mismatches;
 
+    //file pointers for bed
     FILE* f_only_in_a_bed;
     FILE* f_only_in_b_bed;
     FILE* f_mismatches_bed_a;
@@ -83,15 +106,16 @@ typedef struct{
 
 } compare_stat_t;
 
+
 void insert_alignment(sam_stat_t *read, alignment_t *sam_entry){
 
 #ifndef CONSIDER_SUPPLEMENTARY
-    if((sam_entry->flag & (F_UNMAPPED|F_SECONDARY|F_SUPPLEMENTARY))==0){ 
+    if((sam_entry->flag & (F_UNMAPPED|F_SECONDARY|F_SUPPLEMENTARY))==0){ //only primary
 #else
     #ifndef CONSIDER_SECONDARY
-        if((sam_entry->flag & (F_UNMAPPED|F_SECONDARY))==0){ 
+        if((sam_entry->flag & (F_UNMAPPED|F_SECONDARY))==0){ //primary+supplementary
     #else
-        if((sam_entry->flag & (F_UNMAPPED))==0){
+        if((sam_entry->flag & (F_UNMAPPED))==0){ //primary+supplementary+secondary
     #endif
 #endif
         if(read->mapping_of_reads_n>=NUM_MAPPINGS){
@@ -107,11 +131,11 @@ void insert_alignment(sam_stat_t *read, alignment_t *sam_entry){
 }
 
 
+//get the end coordinate of a mapping
 //from https://www.biostars.org/p/17891/
 static inline int32_t cigar_to_aln(char *cigar,int32_t pos){
     char *c = cigar;
     int32_t pos_end=pos;
-    // int32_t n_gap = 0, mlen = 0;
     while(*c!=0){
         char* p2;
         if(!isdigit(*c)){
@@ -127,12 +151,10 @@ static inline int32_t cigar_to_aln(char *cigar,int32_t pos){
             case 'M':
             {
                 pos_end += len; 
-                // mlen += len;
                 break;
             }
             case 'D':
             {
-                // n_gap += len;
                 pos_end += len;
                 break;
             }
@@ -161,6 +183,7 @@ void parse_sam_entry(alignment_t *sam_entry, char *buffer){
         pch = strtok (NULL,"\t\r\n"); assert(pch!=NULL);
         sam_entry->flag = atoi(pch);
 
+        //if mapped
         if(!(sam_entry->flag & 0x04)){        
             sam_entry->strand = (sam_entry->flag&16)? '-' : '+';
 
@@ -180,7 +203,6 @@ void parse_sam_entry(alignment_t *sam_entry, char *buffer){
             pch = strtok (NULL,"\t\r\n");  assert(pch!=NULL);
             sam_entry->target_end = cigar_to_aln(pch,sam_entry->target_start);
 
-            
             //RNEXT
             pch = strtok (NULL,"\t\r\n");  assert(pch!=NULL);
             
@@ -195,7 +217,6 @@ void parse_sam_entry(alignment_t *sam_entry, char *buffer){
             
             //QUAL
             pch = strtok (NULL,"\t\r\n");  assert(pch!=NULL);
-        
         
             //NM
             pch = strtok (NULL,"\t\r\n");  assert(pch!=NULL);
@@ -212,6 +233,7 @@ void parse_sam_entry(alignment_t *sam_entry, char *buffer){
         }
 }
 
+//mapping in 'a' and 'b' are exactly the same?
 static inline int8_t is_correct_exact(alignment_t a, alignment_t b){
     if(a.tid==b.tid && a.target_start==b.target_start && a.strand==b.strand){
         return 1;
@@ -224,6 +246,14 @@ static inline int8_t is_correct_exact(alignment_t a, alignment_t b){
 
 #ifdef OVERLAP_BASED_EVAL 
 //adapted from paftools
+/*Suppose the reported mapping coordinate in samfile 'a' overlap with the 
+mapping coordinate in samfile 'b'  as follows:
+
+mapping in 'a':   --------------------
+mapping in 'b':           ----------------------
+                  |<- l1 ->|<-- o -->|<-- l2 -->|
+Let r=o/(l1+o+l2). The reported mapping is considered correct if r>0.1 by default.
+*/
 #define ovlp_ratio 0.1
 int8_t is_correct_overlap(alignment_t a, alignment_t b)
 {
@@ -242,6 +272,7 @@ int8_t is_correct_overlap(alignment_t a, alignment_t b)
 }
 #endif
 
+//compare 
 void compare_alnread(compare_stat_t *compare, sam_stat_t *reada,sam_stat_t *readb){
     if(reada->mapping_of_reads_n==0 && readb->mapping_of_reads_n==0){
         compare->unmapped_in_both++;
@@ -251,10 +282,12 @@ void compare_alnread(compare_stat_t *compare, sam_stat_t *reada,sam_stat_t *read
     if(readb->mapping_of_reads_n>0 && reada->mapping_of_reads_n==0){
         compare->only_in_b++;    
         alignment_t b = readb->mapping_of_reads[0];
+
+        //tsv - 1 based index
         fprintf(compare->f_only_in_b,"%s\t%s\t%d\t%d\t%d\n",b.rid.c_str(),
         b.tid.c_str(),b.target_start,b.mapq, b.score);
 
-        //0 index start
+        //bed - 0 based index for star coordinate, 1 based index for end coordinate
         fprintf(compare->f_only_in_b_bed,"%s\t%d\t%d\t%s\t%d\t%c\t%d\n",b.tid.c_str(),b.target_start-1,
         b.target_end,b.rid.c_str(),b.score,b.strand,b.mapq);
 
@@ -264,40 +297,45 @@ void compare_alnread(compare_stat_t *compare, sam_stat_t *reada,sam_stat_t *read
     if(reada->mapping_of_reads_n>0 && readb->mapping_of_reads_n==0){
         compare->only_in_a++;
         alignment_t a = reada->mapping_of_reads[0];
+
+        //tsv - 1 based index    
         fprintf(compare->f_only_in_a,"%s\t%s\t%d\t%d\t%d\n",a.rid.c_str(),
         a.tid.c_str(),a.target_start,a.mapq, a.score);
 
-        //0 index start //1 based end
+        //bed - 0 based index for star coordinate, 1 based index for end coordinate
         fprintf(compare->f_only_in_a_bed,"%s\t%d\t%d\t%s\t%d\t%c\t%d\n",a.tid.c_str(),a.target_start-1,
         a.target_end,a.rid.c_str(),a.score,a.strand,a.mapq);
 
         return;
     }
 
+    //compare the mappings for the read in 'a' with the mappings for the same read in 'b'
     int32_t flag=0;
     for(int32_t i=0;i < reada->mapping_of_reads_n ;i++){
         for(int32_t j=0; j < readb->mapping_of_reads_n ;j++){
                 alignment_t a = reada->mapping_of_reads[i];
                 alignment_t b = readb->mapping_of_reads[j];
-                if(a.rid!=b.rid){
+                if(a.rid!=b.rid){ //read names should match
                     cout << a.rid << '\t' << b.rid <<endl;
                 }
                 assert(a.rid==b.rid);
+        //primary only mode        
         #ifndef CONSIDER_SUPPLEMENTARY        
             #ifndef OVERLAP_BASED_EVAL 
-                //same mapping
+                //exact mapping or not?
                 if(is_correct_exact(a,b)){
                     flag++; 
                 }
             #else
+                //coordinates overlap or not?
                 if(is_correct_overlap(a,b)){
                     flag++; 
                 }
             #endif
         #else
-                //in a only take primaries
+                //in 'a' we always take the primary only
                 if((a.flag&(F_SUPPLEMENTARY|F_SECONDARY))==0){
-
+    
                     #ifndef OVERLAP_BASED_EVAL 
                         //same mapping
                         if(is_correct_exact(a,b)){
@@ -331,16 +369,18 @@ void compare_alnread(compare_stat_t *compare, sam_stat_t *reada,sam_stat_t *read
 
     assert(flag==0 || flag ==1);
     if(flag){
-        compare->same++;
+        compare->same++; //correct
     }
     else{
-        compare->mismatches++;
+        compare->mismatches++; //incorrect
         alignment_t a = reada->mapping_of_reads[0];
         alignment_t b = readb->mapping_of_reads[0];
+
+        //1 based index for tsv
         fprintf(compare->f_mismatches,"%s\t%s\t%d\t%d\t%d\t%s\t%d\t%d\t%d\n",a.rid.c_str(),
         a.tid.c_str(),a.target_start,a.mapq, a.score,b.tid.c_str(),b.target_start,b.mapq, b.score);
 
-        //0 index start //1 based end
+        //bed - 0 index start ,1 based end
         fprintf(compare->f_mismatches_bed_a,"%s\t%d\t%d\t%s\t%d\t%c\t%d\n",a.tid.c_str(),a.target_start-1,
         a.target_end,a.rid.c_str(),a.score,a.strand,a.mapq);
         fprintf(compare->f_mismatches_bed_b,"%s\t%d\t%d\t%s\t%d\t%c\t%d\n",b.tid.c_str(),b.target_start-1,
@@ -377,6 +417,8 @@ void print_compare_stat(compare_stat_t *compare){
 #endif
 
 }
+
+//global sam stat update
 void update_sam_stat(sam_stat_t* a,alignment_t curr_a){
     a->num_entries++;
     if(!(curr_a.flag & F_UNMAPPED)){
@@ -396,6 +438,45 @@ void update_sam_stat(sam_stat_t* a,alignment_t curr_a){
     }
             
 }
+
+/*
+Let the 'a' and 'b' be two samfiles for the same set of reads, 
+but mapped with different mappers (or same mapper with different options)
+
+compare sam will compare 'a' and 'b' and give mappings statistics such as the number of reads  :
+- unmapped in both
+- correct : the read maps to the same location (locations overlaps if overlap based evaluation is set)
+- incorrect 
+- read is only mapped in  'a'
+- read is only mapped in  'b'
+- primary mapping in 'a' is a supplementary mapping in 'b'
+- primary mapping in 'b' is a secondary mappings in 'b'
+
+compare sam will also output (as tsv files and bed file) reads that :
+-mismatch between 'a' and 'b' 
+-unique to 'a'
+-unique to 'b'
+
+functionality : 
+
+samfile 'a' and 'b' are sequentially read while loading all the mappings for a 
+particular read name at a time. 
+IMPORTANT : the samfiles 'a' and 'b' should have the reads in the same order and
+the multiple mappings for a given read ID should be adjacently located.
+
+For each loaded read, we compare the mappings between 'a' and 'b'
+In 'a' always the primary mapping is considered despite the value of 
+CONSIDER_SUPPLEMENTARY and CONSIDER_SECONDARY flags.
+In 'b' supplementary and secondary mappings can also be considered 
+if the flags CONSIDER_SUPPLEMENTARY and CONSIDER_SECONDARY are set.
+
+The comparison statistics are updated during the comparison and 
+the entries will be written by the tsv and bed file if required.
+
+Finally we print the comparison statistics.
+
+*/
+
 
 void comparesam(int argc, char* argv[]){
 	
@@ -473,7 +554,7 @@ void comparesam(int argc, char* argv[]){
 
     int8_t state=0;
 
-    /** Read SAM A*/
+    /** Read all mappings for the cuurrent read in SAM A*/
 	while(1){
         readlinebytes_a=getline(&buffer_a, &bufferSize_a, samfile_a); 
         if(readlinebytes_a == -1){
@@ -515,7 +596,7 @@ void comparesam(int argc, char* argv[]){
 
             int8_t break_flag = 0;
 
-            /** Now read the entries for the readID for SAMA, from SAM B */   
+            /** Now read the entries for the same read in SAM B */   
             while(1){
                 readlinebytes_b=getline(&buffer_b, &bufferSize_b, samfile_b); 
                 if(readlinebytes_b == -1){
@@ -552,7 +633,7 @@ void comparesam(int argc, char* argv[]){
 
             }
 
-            //do the comparson for the mappings in the read    
+            //do the comparison for the mappings in the read    
             if(state==1){
                 compare_alnread(&compare,&a,&b);  
                 state=0;
