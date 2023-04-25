e3f9187130c8630aa5620025d51d5f059d1e4a30
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2023-04-25 11:07:40 +1000

    identity scripts print the count

--- bin/identityrna.sh
+++ bin/identityrna.sh
@@ -18,6 +18,6 @@ test -e $FASTQ || die "$FASTQ does not exist"
 minimap2 --version > /dev/null || die "minimap2 not found in PATH"
 datamash --version > /dev/null || die "datamash not found in PATH"
 
-OUTPUT=$(minimap2 -cx splice -uf -k14  $1 -t8 --secondary=no ${FASTQ} | awk '{print $10/$11}' | datamash mean 1 sstdev 1 q1 1 median 1 q3 1)
-echo -en "sample\tmean\tsstdev\tq1\tmedian\tq3\n"
+OUTPUT=$(minimap2 -cx splice -uf -k14  $1 -t8 --secondary=no ${FASTQ} | awk '{print $10/$11}' | datamash mean 1 sstdev 1 q1 1 median 1 q3 1 count 1)
+echo -en "sample\tmean\tsstdev\tq1\tmedian\tq3\tn\n"
 echo -en "${FASTQ}\t$OUTPUT\n"
